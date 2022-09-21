f2e9274e5eef54aaf39689f7706bb008177848b2
dev-th-kang <kang.1107.th@gmail.com>
2022-09-21 21:35:48 +0900

    update 2022-09-21

--- fmanagement/checkfolder.py
+++ fmanagement/checkfolder.py
@@ -1,2 +1,13 @@
 ##project가 있는 폴더가 수정됬는지 여부 판단
 ##git init 이 되어있는 작업폴더여야함.
+import subprocess
+ 
+data = subprocess.check_output(['git', 'diff'])
+l = str(data).split('\\n')
+isCheck = False
+for c in l:
+    check = '+++' in c
+    if check == True:
+        isCheck = True
+        break
+print(isCheck)
